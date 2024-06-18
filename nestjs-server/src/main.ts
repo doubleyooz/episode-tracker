8cda2e682b2c46b28dc479729861c92b5ca0f813
Joanderson <sirjoandersonwf@gmail.com>
2024-06-18 10:19:30 -0300

    fix: cors

--- nestjs-server/src/main.ts
+++ nestjs-server/src/main.ts
@@ -9,15 +9,34 @@ import {
 } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
+  const CORS_OPTIONS = {
+    origin: [process.env.CLIENT], // or '*' or whatever is required
+    allowedHeaders: [
+      'Access-Control-Allow-Origin',
+      'Origin',
+      'X-Requested-With',
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ],
+    exposedHeaders: 'Authorization',
+    credentials: true,
+    methods: ['GET', 'PUT', 'OPTIONS', 'POST', 'DELETE'],
+  };
+  const adapter = new FastifyAdapter();
+  adapter.enableCors(CORS_OPTIONS);
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    adapter,
   );
   const appConfig: ConfigService = app.get(ConfigService);
+
   app.use(cookieParser());
-  app.enableCors({ origin: appConfig.get('CLIENT'), credentials: true });
+
+  // app.enableCors({ origin: appConfig.get('CLIENT'), credentials: true });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(appConfig.get<number>('PORT'));
+  await app.listen(appConfig.get<number>('PORT'), '0.0.0.0');
 }
 bootstrap();
